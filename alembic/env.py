34f747d8449fdee6a16d05e5b732523433211236
rafael rotiroti <rotirotirafa@gmail.com>
2023-12-21 14:21:14 -0300

    config

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,14 @@
+import os
 from logging.config import fileConfig
 
+from dotenv import load_dotenv
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+# Carrega variáveis de ambiente do arquivo .env
+load_dotenv()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -57,6 +62,10 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    # Configuração do SQLAlchemy
+    config = context.config
+    url = os.getenv("DATABASE_URL")
+    config.set_main_option("sqlalchemy.url", url)
     connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
